f6238904ddc9a10ee4a44acdbad163f5100a1d81
cudba <1487984+cudba@users.noreply.github.com>
2026-03-25 07:50:11 +0100

    bump version

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default ({ config }) => {
     name: "coltivio",
     slug: "coltivio",
     owner: "agricoltivio",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: process.env.SCHEME,
